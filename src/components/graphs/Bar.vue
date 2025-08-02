4fa06ecd98cffb67fe6621c12854578a0f8404b5
etousoftdev1 <developer_1@etousoft.com>
2025-08-02 21:30:48 +0800

    Feat : update

--- src/components/graphs/Bar.vue
+++ src/components/graphs/Bar.vue
@@ -1,10 +1,14 @@
 <template>
-  <apexchart
-    :type="chartType"
-    height="350"
-    :options="chartOptions"
-    :series="series"
-  />
+  <div style="width: 100%;">
+    <apexchart
+      :type="chartType"
+      height="400"
+      :width="'100%'"
+      :options="chartOptions"
+      :series="series"
+    />
+  </div>
+
 </template>
 
 <script>
@@ -17,14 +21,20 @@ export default {
       chartType: 'bar',  // changed from 'bar' to 'line'
       series: [{
         name: 'Sales',
-        data: [30, 40, 35, 50, 49, 60, 70, 91]
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 80, 90, 100]
       }],
       chartOptions: {
         chart: {
           id: 'vuechart-example'
         },
         xaxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
+          categories: ['2025-01', '2025-02', '2025-03', '2025-04', '2025-05', '2025-06', '2025-07', '2025-08', '2025-09', '2025-10', '2025-11', '2025-12'],
+          labels: {
+            rotate: -45,         // 👈 rotate labels diagonally (negative is upward)
+            style: {
+              fontSize: '12px'
+            }
+          }
         }
       }
     }
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,10 +1,168 @@
 <template>
-  <v-card class="ma-4">
-    <v-card-title>Dashboard Information</v-card-title>
-    <v-card-text>
-     <Bar/>
-    </v-card-text>
-  </v-card>
+  <div>
+    <div class="d-flex align-center" style="gap: 10px; padding: 15px;">
+      <v-card width="50%" style="height: 50vh;">
+        <v-card-title>
+          <v-icon class="mr-2">mdi-home</v-icon>
+          Rent Statistics
+        </v-card-title>
+        <v-card-text>
+         <div class="d-flex flex-wrap rent-statistics-wrapper">
+           <v-card style="flex: 1; background-color: green;">
+            <v-card-title>
+             Done
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex align-center" style="gap: 10px;">
+                <v-icon>mdi-check</v-icon>
+                <h2>15</h2>
+              </div>
+            </v-card-text>
+          </v-card>
+           <v-card style="flex: 1; background-color: blue;">
+            <v-card-title>
+             Processing
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex align-center" style="gap: 10px;">
+                <v-icon>mdi-cog-sync</v-icon>
+                <h2>5</h2>
+              </div>
+            </v-card-text>
+          </v-card>
+           <v-card style="flex: 1; background-color: orange;">
+            <v-card-title>
+             Pending
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex align-center" style="gap: 10px;">
+                <v-icon>mdi-progress-clock</v-icon>
+                <h2>10</h2>
+              </div>
+            </v-card-text>
+          </v-card>
+         </div>
+        </v-card-text>
+      </v-card>
+      <v-card width="50%" style="height: 50vh;">
+        <v-card-title>
+          <v-icon class="mr-2">mdi-chart-line</v-icon>
+          Site Statistics
+        </v-card-title>
+        <v-card-text>
+         <div class="d-flex flex-wrap rent-statistics-wrapper">
+           <v-card style="width: 40%; background-color: gray;">
+            <v-card-title>
+             Total Users
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex align-center" style="gap: 10px;">
+                <v-icon>mdi-account-multiple</v-icon>
+                <h2>30</h2>
+              </div>
+            </v-card-text>
+          </v-card>
+           <v-card style="width: 40%; background-color: gray;">
+            <v-card-title>
+             Processing
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex align-center" style="gap: 10px;">
+                <v-icon>mdi-key</v-icon>
+                <h2>12</h2>
+              </div>
+            </v-card-text>
+          </v-card>
+           <v-card style="width: 40%; background-color: gray;">
+            <v-card-title>
+             Total Earnings
+            </v-card-title>
+            <v-card-text>
+              <div class="d-flex align-center" style="gap: 10px;">
+                <h2>₱ 15000</h2>
+              </div>
+            </v-card-text>
+          </v-card>
+         </div>
+        </v-card-text>
+      </v-card>
+    </div>
+    <div class="mb-4 mx-4">
+      <v-card >
+        <v-card-title class="d-flex align-center justify-space-between">
+          <div style="flex: 1;">
+            <v-icon class="mr-2">mdi-chart-bar</v-icon>
+            Rent Statistics
+          </div>
+          <div class="d-flex align-center" style="gap: 10px;">
+              <v-menu
+                ref="menu1"
+                v-model="menu1"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="auto"
+                
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateFormatted"
+                    label="Date From"
+                    persistent-hint
+                    style="width: 290px;"
+                    prepend-icon="mdi-calendar"
+                    v-bind="attrs"
+                    @blur="date = parseDate(dateFormatted)"
+                    v-on="on"
+                    dense
+                    hide-details
+                    outlined
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  no-title
+                  @input="menu1 = false"
+                ></v-date-picker>
+              </v-menu>
+              <v-menu
+                v-model="menu2"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateFormatted"
+                    label="Date To"
+                    persistent-hint
+                    style="width: 290px;"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    hide-details
+                    v-bind="attrs"
+                    v-on="on"
+                    dense
+                    outlined
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  no-title
+                  @input="menu2 = false"
+                ></v-date-picker>
+              </v-menu>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <Bar/>
+        </v-card-text>
+      </v-card>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,7 +171,11 @@
     components: {
       Bar,
     },
-    data: () => ({
+    data: () => vm =>({
+      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+      dateFormatted: vm.formatDate((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)),
+      menu1: false,
+      menu2: false,
       desserts: [
         {
           name: 'Frozen Yogurt',
@@ -110,6 +272,11 @@
         { text: 'Iron (%)', value: 'iron' },
       ],
     }),
+  computed: {
+    computedDateFormatted () {
+      return this.formatDate(this.date)
+    },
+  },
   mounted() {
       // this.$awn.success('Notification is working!')
   },
@@ -121,3 +288,12 @@
   },
   }
 </script>
+
+<style>
+  .rent-statistics-wrapper {
+    gap: 20px;
+  }
+  .rent-statistics-wrapper * {
+    color: #fff !important;
+  }
+</style>
